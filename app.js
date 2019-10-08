5c18553e34f7f604288309f22164b0b640261f19
NixonPixel <nerdik5@gmail.com>
2019-10-08 12:38:04 +0300

    change package.json

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ console.log( path.resolve(
 ))
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'))
+    app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
         res.sendFile(
